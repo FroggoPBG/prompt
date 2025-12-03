12acc7761ce60dda49c01f30855acbbe1933dc8b
FroggoPBG <charmayshcs@gmail.com>
2025-12-03 16:10:20 +0800

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,27 @@
 """Components package for Legal Tech Sales Prospecting Tool."""
-# This file makes the components directory a Python package.
-# Import statements are handled in app.py directly.
+
+from .email_templates import EmailTemplate, EmailTemplateGenerator
+from .presets import ProspectPreset, export_preset_bytes, load_preset_into_state
+from .recipes import ProspectContext, PromptRecipeManager
+from .writing_checker import WritingAnalysis, WritingIssue, check_plain_english, get_writing_tips
+
+__all__ = [
+    # Email templates
+    "EmailTemplate",
+    "EmailTemplateGenerator",
+    
+    # Presets
+    "ProspectPreset",
+    "export_preset_bytes",
+    "load_preset_into_state",
+    
+    # Recipes
+    "ProspectContext",
+    "PromptRecipeManager",
+    
+    # Writing checker
+    "WritingAnalysis",
+    "WritingIssue",
+    "check_plain_english",
+    "get_writing_tips",
+]
